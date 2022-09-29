4b97fda33ac4cec31510627d952c25de8652c99d
ESN <nicolas.esseiva@gmail.com>
2022-09-29 16:22:16 +0200

    Suite ex1-1

---
+++ Ex1/Ex1_1.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+int main(void)
+{
+    printf("Hello World\n");
+
+/*
+    uint32_t x = 2, y = 9;
+
+    uint8_t z = x + y;
+
+    printf("Result is %d \n", z);
+    printf("y / x = %d \n", y/x);
+
+    unsigned char b = 5;
+    short s = 8;
+    int i = 9;
+    float f = 2.5;
+    double d = 5.2;
+
+    printf("%f \n",d/2);
+    printf("%d \n",s*4);
+    printf("%d \n",(b+s) > (5*f));
+    printf("%f \n",i/4 + d);
+    printf("%d \n",1/3);
+    printf("%d \n",b&6);
+
+*/
+
+    int j = 7, k = 4;
+    bool a = true, b = true;
+
+
+    return 0;
+}
