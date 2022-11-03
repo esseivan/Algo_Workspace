355ae1c377c29edfc2a22e25b9edffcf17ec7f04
ESN <nicolas.esseiva@gmail.com>
2022-11-03 15:20:10 +0100

    Update Ex1_5.c

--- Ex1/Ex1_5.c
+++ Ex1/Ex1_5.c
@@ -1,9 +1,12 @@
 // Ex 1.5
+// Bubble sort
 
 #include <stdio.h>
 #include <stdint.h>
 
 int BubbleSort(uint16_t *A, int n);
+void BubbleSort2(uint16_t *Tab, int n, int* permut);
+int BubbleSort_Arrays(uint16_t Tab[], int n);
 void PrintTableau(uint16_t *Tab, int n);
 void PrintTableau_Tab(uint16_t Tab[], int n); // Meme chose que l'autre fonction mais sous forme tableau
 void swap(uint16_t *a, uint16_t *b);
@@ -18,7 +21,10 @@ int main(void)
     PrintTableau(Tab, n);
     printf("\n");
 
+    // int permutations = BubbleSort_Arrays(Tab, n);
     int permutations = BubbleSort(Tab, n);
+    // int permutations;
+    // BubbleSort2(Tab, n, &permutations);
 
     printf("Tableau trie : ");
     PrintTableau(Tab, n);
@@ -64,6 +70,50 @@ int BubbleSort(uint16_t *Tab, int n)
             if (*(Tab + i) < *(Tab + i - 1)) // Dereferenciation
             {
                 swap((Tab + i), (Tab + i - 1)); // Envoi des adresses
+                permutations++;
+                s++;
+            }
+        }
+
+    } while (s != 0);
+
+    return permutations;
+}
+
+void BubbleSort2(uint16_t *Tab, int n, int* permut)
+{
+    int permutations = 0;
+    int s = 0;
+    do
+    {
+        s = 0;
+        for (int i = 1; i < n; i++)
+        {
+            if (*(Tab + i) < *(Tab + i - 1)) // Dereferenciation
+            {
+                swap((Tab + i), (Tab + i - 1)); // Envoi des adresses
+                permutations++;
+                s++;
+            }
+        }
+
+    } while (s != 0);
+
+    *permut = permutations;
+}
+
+int BubbleSort_Arrays(uint16_t Tab[], int n)
+{
+    int permutations = 0;
+    int s = 0;
+    do
+    {
+        s = 0;
+        for (int i = 1; i < n; i++)
+        {
+            if (Tab[i] < Tab[i-1]) // Dereferenciation
+            {
+                swap(&Tab[i], &Tab[i-1]); // Envoi des adresses
                 permutations++;
                 s++;
             }
