259d763f11679e4957ab300d99188a48290b730a
EsseivaN <nicolas.esseiva@gmail.com>
2022-11-06 11:27:01 +0100

    ex 1.6 snake case + float format fix

--- Ex1/Ex1_6.c
+++ Ex1/Ex1_6.c
@@ -1,9 +1,12 @@
+// Converter Celsius to Fahrenheit
+
+
 #include <stdio.h>
 #include <stdint.h>
 
-float converter(float valD)
+float convert_c_to_f(float val_c)
 {
-    return (valD * 1.8) + 32.0;
+    return (val_c * 1.8) + 32.0;
 }
 
 int main(void)
@@ -13,19 +16,19 @@ int main(void)
     printf("%cC to %cF converter !\n", cDeg, cDeg);
     // N'imprime pas les ° pour certaines raisons inconnues ???
 
-    float valD = 10.0;
-    float valF = valD * 1.8 + 32.0;
+    float val_c = 10.0;
+    float val_f = val_c * 1.8 + 32.0;
 
-    // printf("Value of %.2f%cC corresponds to %.2f%cF\r", valD, cDeg, valF, cDeg);
+    // printf("Value of %.2f%cC corresponds to %.2f%cF\r", val_c, cDeg, val_f, cDeg);
 
     // b
-    valF = converter(valD);
+    val_f = convert_c_to_f(val_c);
 
     // c
     for (int i = -20; i <= 30; i++)
     {
-        valF = converter((float)i);
-        printf("Value of %+02.1f%cC corresponds to %+2.1f%cF\n", (float)i, cDeg, valF, cDeg);
+        val_f = convert_c_to_f((float)i);
+        printf("Value of %+5.1f%cC corresponds to %+5.1f%cF\n", (float)i, cDeg, val_f, cDeg);
     }
 
     return 0;
