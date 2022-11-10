59d3ee135c4b103fc784dc88a29e822e1290c8f9
ESN <nicolas.esseiva@gmail.com>
2022-11-10 15:39:37 +0100

    Fin ex 1_7

--- S1/Ex1_7.c
+++ S1/Ex1_7.c
@@ -6,64 +6,78 @@
 #include <time.h>
 #include <limits.h>
 #include <string.h>
+#include <assert.h>
+
+#define DATE_TIME_FORMAT "%02d.%02d.%04d %02d:%02d:%02d"
 
 // c)
 // Affiche le texte et la date
 void PrintTime(char *text, struct tm *const val)
 {
-    if (val == 0 || val == NULL)
-    {
-        printf("Invalid pointer !\n");
-        return;
-    }
+    assert(val != 0);
+    assert(text != 0);
     printf(text,
            val->tm_mday, val->tm_mon + 1, val->tm_year + 1900,
            val->tm_hour, val->tm_min, val->tm_sec);
 }
 
-char *GetWeekDay(int dayNum)
+char const *GetWeekDay(int dayNum)
 {
     // tm_wday : Days since Sunday (0-6)
 
     // Une autre methode est avec un tableau mais cela utilise
     // de la memoire ram alors qu'ici uniquement de la memoire programme
 
-    switch (dayNum)
-    {
-    case 0:
-        return "Dimanche";
-    case 1:
-        return "Lundi";
-    case 2:
-        return "Mardi";
-    case 3:
-        return "Mercredi";
-    case 4:
-        return "Jeudi";
-    case 5:
-        return "Vendredi";
-    case 6:
-        return "Samedi";
-    default:
-        return "Undefined";
-    }
+    // switch (dayNum)
+    // {
+    // case 0:
+    //     return "Dimanche";
+    // case 1:
+    //     return "Lundi";
+    // case 2:
+    //     return "Mardi";
+    // case 3:
+    //     return "Mercredi";
+    // case 4:
+    //     return "Jeudi";
+    // case 5:
+    //     return "Vendredi";
+    // case 6:
+    //     return "Samedi";
+    // default:
+    //     return "Undefined";
+    // }
+
+    char const *const pDayOfWeekName[] =
+        {"Dimanche",
+         "Lundi",
+         "Mardi",
+         "Mercredi",
+         "Jeudi",
+         "Vendredi",
+         "Samedi"};
+
+    return pDayOfWeekName[dayNum];
 }
 
 int main(void)
 {
     // a)
     // recuperer le temps
-    int64_t const cTimeSec = time(NULL);
+    time_t const cTimeSec = time(NULL);
 
     // convertir dans la structure tm
     struct tm *const pCurrTime = localtime(&cTimeSec);
-    PrintTime("a) Current date and time is\t: %02d.%02d.%04d %02d:%02d:%02d\n", pCurrTime);
+    PrintTime("a) Current date and time is\t: \n", pCurrTime);
 
     // b)
     // Temps maximum
-    int64_t const cTimeMaxSec = INT32_MAX;
+    time_t const cTimeMaxSec = UINT32_MAX * 7LL;
     // convertir dans la structure tm
     struct tm *const pMaXTime = localtime(&cTimeMaxSec);
+
+    struct tm *const pMaxTime2 = NULL;
+    localtime_s(pMaxTime2, &cTimeSec);
 
     PrintTime("b) Overflow will occur 1s after\t: %02d.%02d.%04d %02d:%02d:%02d\n", pMaXTime);
 
