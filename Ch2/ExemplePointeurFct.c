4979a4df0e22f419c48907f70ed1a416cf7a78b5
ESN <nicolas.esseiva@gmail.com>
2022-11-17 16:12:06 +0100

    Exemple pointeur de fonctions

---
+++ Ch2/ExemplePointeurFct.c
@@ -0,0 +1,72 @@
+// Exemple d'utilisation de pointeurs de fonctions
+//
+
+#include <stdio.h>
+
+// Fonctions d'opération
+long int mult(int val1, int val2);
+long int div(int val1, int val2);
+long int add(int val1, int val2);
+long int sub(int val1, int val2);
+
+// Fonctions d'affichage
+void print_mult(int val1, int val2);
+void print_div(int val1, int val2);
+void print_add(int val1, int val2);
+void print_sub(int val1, int val2);
+
+typedef long int (*pFctOp)(int, int);
+typedef void (*pFctPrintOp)(int, int);
+pFctOp TabFctOp[] = {mult, div, add, sub};
+pFctPrintOp TabFctPrint[] = {print_mult, print_div, print_add, print_sub};
+// pFctOp TabFct[] = {&mult, &div, &add, &sub};
+int FctNb = sizeof(TabFctOp) / sizeof(TabFctOp[0]);
+
+int main(void)
+{
+    int x = 360;
+    int y = 5;
+    for (int i = 0; i < FctNb; i++)
+    {
+        // pFctOp pointeurFct = TabFct[i];
+        long int result = TabFctOp[i](x, y);
+        TabFctPrint[i](x, y);
+        printf(" = %ld\n", result);
+    }
+
+    return 0;
+}
+
+long int mult(int val1, int val2)
+{
+    return (long int)val1 * (long int)val2;
+}
+long int div(int val1, int val2)
+{
+    return (long int)val1 / (long int)val2;
+}
+long int add(int val1, int val2)
+{
+    return (long int)val1 + (long int)val2;
+}
+long int sub(int val1, int val2)
+{
+    return (long int)val1 - (long int)val2;
+}
+
+void print_mult(int val1, int val2)
+{
+    printf("%d * %d", val1, val2);
+}
+void print_div(int val1, int val2)
+{
+    printf("%d / %d", val1, val2);
+}
+void print_add(int val1, int val2)
+{
+    printf("%d + %d", val1, val2);
+}
+void print_sub(int val1, int val2)
+{
+    printf("%d - %d", val1, val2);
+}
