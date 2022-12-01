b05fd23d6a85a6e5497f2783d59e7d319915cc69
ESN <nicolas.esseiva@gmail.com>
2022-12-01 16:01:49 +0100

    ex 2.4 b

--- S2/ex2_4_b.c
+++ S2/ex2_4_b.c
@@ -6,22 +6,25 @@ enum
     eArgumentSize = 80
 };
 
+double myfunc(char *Text)
+{
+    char Argument[eArgumentSize];
+    printf("%s", Text);
+    scanf("%s", Argument);
+    printf("\n");
+    return atof(Argument);
+}
+
 int main(void)
 {
     printf("Welcome ! Enter two number to be added together.\n");
 
-    char Argument1[eArgumentSize];
-    char Argument2[eArgumentSize];
-
-    printf("\nPlease enter first operand : ");
-    scanf("%s", Argument1);
-
-    printf("\nPlease enter second operand : ");
-    scanf("%s", Argument2);
+    char Text1[] = "\nPlease enter first operand : ";
+    char Text2[] = "\nPlease enter second operand : ";
 
     // atof convertit un string en un double. Retourne 0 si erreur
-    double Number1 = atof(Argument1);
-    double Number2 = atof(Argument2);
+    double Number1 = myfunc(Text1);
+    double Number2 = myfunc(Text2);
     double Result = Number1 + Number2;
 
     printf("\nResult of %f + %f = %f", Number1, Number2, Result);
